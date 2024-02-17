20cdb052d92c4c8930af3491ddd62ed7d2bb6eb1
Moro Bamber <99629944+UnderYourSpell@users.noreply.github.com>
2024-02-16 19:19:34 -0900

    First
    First

---
+++ buffer_overflows/buf1.c
@@ -0,0 +1,14 @@
+//test program
+#include <stdio.h>
+
+int overflow(char* input1){
+    char buff[16]; //stack buffer of 16 bytes
+    strcpy(buff,input1);
+    return 0;
+}
+
+void main(int argc, char* argv[]){
+    overflow(argv[1]);
+}
+
+// ./a.out `python -c 'print "A" * 32'` as example input to cause overflow
---
+++ buffer_overflows/heap_ovr_1.c
@@ -0,0 +1,31 @@
+//example of a heap overflow
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define password "password"
+
+checkpw(){
+    char pw[100]; //give pw a 100 byte buffer
+    printf("Please enter the password: ");
+    gets(pw); //unsafe gets function -> no size argument
+    if (strcmp(pw, password) == 0) //compare pw to password, if 0, match
+        granted();
+    else
+        printf("Access denied\n");
+}
+
+granted() {
+    printf("Access granted\n");
+    return 0;
+}
+
+int main(int argc, char* argv[]){ //v0 
+    setuid(1000);
+    if(argc > 1){ //argument entered, don't accept, we don't have args
+            printf("There is no usage\n");
+            exit(1);
+    }
+    checkpw();
+    return 0;
+}
